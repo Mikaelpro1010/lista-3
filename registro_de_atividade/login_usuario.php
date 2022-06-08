d54b031a29bb52b89f16bf39519d0ffb09bdc244
Mikael <mkpro10@alu.ufc.br>
2022-06-08 07:57:41 -0300

    adicionado nova questao da lista 3

---
+++ registro_de_atividade/login_usuario.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form class="row g-3" action="gravar_session.php" method="GET">
+    <div class="row col-2 g-3 mx-auto">
+        <label for="login" class="form-label">Informe o seu nome:</label>
+        <input type="text" name="login" id="login" class="form-control">
+    
+    <input class="btn btn-success" type="submit" value="Enviar">
+    </div>
+</body>
+</html>
